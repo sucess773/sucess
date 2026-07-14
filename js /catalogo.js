493ae2c9e1c5ba7c9a6698cc6b66af7f02117c94
sucess773 <jorgitooficial67@gmail.com>
2026-07-14 15:46:02 -0500

    Update catalogo.js

--- js /catalogo.js
+++ js /catalogo.js
@@ -1,32 +1,27 @@
 const productos = [
-
 {
 nombre:"Panini Mundial 2026",
 precio:2,
 imagen:"assets/productos/panini1.jpg"
 },
-
 {
 nombre:"Panini Qatar",
 precio:2,
 imagen:"assets/productos/panini2.jpg"
 },
-
 {
 nombre:"Tres Reyes Liga 1",
 precio:1.5,
 imagen:"assets/productos/tresreyes1.jpg"
 },
-
 {
 nombre:"Tres Reyes Copa América",
 precio:2,
 imagen:"assets/productos/tresreyes2.jpg"
 }
-
 ];
 
-const contenedor=document.getElementById("productos");
+const contenedor = document.getElementById("productos");
 
 function mostrar(lista){
 
@@ -34,28 +29,19 @@ contenedor.innerHTML="";
 
 lista.forEach(p=>{
 
-contenedor.innerHTML+=`
-
+contenedor.innerHTML += `
 <div class="producto">
-
-<img src="${p.imagen}">
-
+<img src="${p.imagen}" alt="${p.nombre}">
 <div class="info">
-
 <h3>${p.nombre}</h3>
-
-<div class="precio">
-S/ ${p.precio}
-</div>
+<div class="precio">S/ ${p.precio}</div>
 
 <button onclick="agregarAlCarrito('${p.nombre}', ${p.precio}, '${p.imagen}')">
 Agregar al carrito
 </button>
 
 </div>
-
 </div>
-
 `;
 
 });
@@ -64,14 +50,14 @@ Agregar al carrito
 
 mostrar(productos);
 
-document.getElementById("buscador").addEventListener("keyup",e=>{
+document.getElementById("buscador").addEventListener("keyup",function(e){
 
 const texto=e.target.value.toLowerCase();
 
-mostrar(productos.filter(p=>
+mostrar(productos.filter(function(p){
 
-p.nombre.toLowerCase().includes(texto)
+return p.nombre.toLowerCase().includes(texto);
 
-));
+}));
 
 });
