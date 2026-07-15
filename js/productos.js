61fa024708874124106a1629470f55190ff87fc1
sucess773 <jorgitooficial67@gmail.com>
2026-07-14 21:33:52 -0500

    Create productos.js

---
+++ js/productos.js
@@ -0,0 +1,242 @@
+const productos = [
+
+{
+id:1,
+pais:"Uzbekistán",
+codigo:"UZB19",
+nombre:"Igor Sergeev",
+precio:1.50,
+stock:1,
+imagen:"assets/productos/UZB19.jpg"
+},
+
+{
+id:2,
+pais:"España",
+codigo:"ESP20",
+nombre:"Mikel Oyarzabal",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/ESP20.jpg"
+},
+
+{
+id:3,
+pais:"España",
+codigo:"ESP1",
+nombre:"Escudo",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/ESP1.jpg"
+},
+
+{
+id:4,
+pais:"España",
+codigo:"ESP2",
+nombre:"Unai Simón",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/ESP2.jpg"
+},
+
+{
+id:5,
+pais:"Paraguay",
+codigo:"PAR20",
+nombre:"Antonio Sanabria",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/PAR20.jpg"
+},
+
+{
+id:6,
+pais:"Paraguay",
+codigo:"PAR12",
+nombre:"Andrés Cubas",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/PAR12.jpg"
+},
+
+{
+id:7,
+pais:"Paraguay",
+codigo:"PAR19",
+nombre:"Ángel Romero",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/PAR19.jpg"
+},
+
+{
+id:8,
+pais:"Paraguay",
+codigo:"PAR4",
+nombre:"Gustavo Gómez",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/PAR4.jpg"
+},
+
+{
+id:9,
+pais:"Canadá",
+codigo:"CAN7",
+nombre:"Derek Cornelius",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/CAN7.jpg"
+},
+
+{
+id:10,
+pais:"Canadá",
+codigo:"CAN14",
+nombre:"Jacob Shaffelburg",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/CAN14.jpg"
+},
+
+{
+id:11,
+pais:"Canadá",
+codigo:"CAN19",
+nombre:"Cyle Larin",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/CAN19.jpg"
+},
+
+{
+id:12,
+pais:"Croacia",
+codigo:"CRO20",
+nombre:"Franjo Ivanovic",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/CRO20.jpg"
+}
+  ,
+
+{
+id:13,
+pais:"Escocia",
+codigo:"SCO4",
+nombre:"Kieran Tierney",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SCO4.jpg"
+},
+
+{
+id:14,
+pais:"Escocia",
+codigo:"SCO10",
+nombre:"Grant Hanley",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SCO10.jpg"
+},
+
+{
+id:15,
+pais:"Escocia",
+codigo:"SCO15",
+nombre:"Ryan Christie",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SCO15.jpg"
+},
+
+{
+id:16,
+pais:"Escocia",
+codigo:"SCO20",
+nombre:"Ben Doak",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SCO20.jpg"
+},
+
+{
+id:17,
+pais:"Senegal",
+codigo:"SEN6",
+nombre:"Ismail Jakobs",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SEN6.jpg"
+},
+
+{
+id:18,
+pais:"Senegal",
+codigo:"SEN9",
+nombre:"Idrissa Gana Gueye",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SEN9.jpg"
+},
+
+{
+id:19,
+pais:"Senegal",
+codigo:"SEN13",
+nombre:"Equipo de Senegal",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/SEN13.jpg"
+},
+
+{
+id:20,
+pais:"Cabo Verde",
+codigo:"CPV12",
+nombre:"Jamiro Monteiro",
+precio:2.00,
+stock:2,
+imagen:"assets/productos/CPV12.jpg"
+},
+
+{
+id:21,
+pais:"Cabo Verde",
+codigo:"CPV17",
+nombre:"Ryan Mendes",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/CPV17.jpg"
+},
+
+{
+id:22,
+pais:"México",
+codigo:"MEX3",
+nombre:"Johan Vásquez",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/MEX3.jpg"
+},
+
+{
+id:23,
+pais:"México",
+codigo:"MEX7",
+nombre:"Israel Reyes",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/MEX7.jpg"
+},
+
+{
+id:24,
+pais:"México",
+codigo:"MEX18",
+nombre:"Alexis Vega",
+precio:2.00,
+stock:1,
+imagen:"assets/productos/MEX18.jpg"
+}
